f7e2dc3cb8e6ff8e36b1de37f80529949d45c895
MoonSuhan <moonsuhan@MoonSuhans-MacBook-Pro.local>
2016-01-14 23:16:26 +0900

    adsf

---
+++ src/android/MediaPlus.java
@@ -0,0 +1,27 @@
+package com.reconers.mediaplus;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+/**
+ * Created by moonsuhan on 2016. 1. 14..
+ */
+public class MediaPlus extends CordovaPlugin {
+
+
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+    }
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        return super.execute(action, args, callbackContext);
+    }
+
+
+}
